797e6d3d513f9985b06b515dc9cdf9241a23aff9
João Cruz <joaocruz375@gmail.com>
2020-08-14 23:12:10 -0300

    ajustes no carrossel

--- src/components/Caroussel/index.jsx
+++ src/components/Caroussel/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import Slider from "react-slick";
+//import Slider from "react-slick";
 
 import './styles.css'
 
 import carrousselImg01 from '../../assets/images/carroussel.png'
-import carrousselImg03 from '../../assets/images/carroussel02.jpeg'//editar imagem (quebrou o layout)
-import carrousselImg02 from '../../assets/images/carroussel03.jpeg'//editar imagem (quebrou o layout)
+//import carrousselImg02 from '../../assets/images/carroussel02.jpeg'//editar imagem (quebrou o layout)
+//import carrousselImg03 from '../../assets/images/carroussel03.jpeg'//editar imagem (quebrou o layout)
 
 function Caroussel() {
     return (
         <div className="carroussel">
             <section>
                 <ul>
+
                     <li>
                         <img src={carrousselImg01} alt="Hamburguer 1" />
                     </li>
-
                 </ul>
             </section>
         </div>

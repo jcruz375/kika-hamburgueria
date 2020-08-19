e2bfd64ca8e1f07fa9e1d6d0df4846b64cdaefe6
João Vitor Oliveira Cruz <61796057+jcruz375@users.noreply.github.com>
2020-08-19 15:21:47 -0300

    adcionando link para o whatsapp

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import gpsIcon from '../../assets/images/gps.svg'
-import facebookIcon from '../../assets/images/facebook.svg'
+import whatsAppIcon from '../../assets/images/whatsapp.svg'
 import instagramIcon from '../../assets/images/instagram.svg'
 import './styles.css';
 
@@ -13,12 +13,12 @@ function Footer() {
                     <img src={gpsIcon} alt="icone de gps"/>
                 </span>
                 <div className="footer-texts">
-                    <p>Avenida Orfeo Paravente, 182</p>
+                    <p>Rua Villar Torpim, 55</p>
                     <p>(11)96802-5673</p>
-                    <p>kikahamburgueria@gmail.com</p>
+                    <p>hamburgueriakika@gmail.com</p>
                 </div>
                 <div className="footer-links">
-                    <a target="_blank"  rel="noopener noreferrer" href="http://api.whatsapp.com/send?1=pt_BR&phone=5511968025673"><img src={facebookIcon} alt="Icone link do facebook"/></a>
+                    <a target="_blank"  rel="noopener noreferrer" href="http://api.whatsapp.com/send?1=pt_BR&phone=5511968025673"><img src={whatsAppIcon} alt="Icone link do facebook"/></a>
                     <a target="_blank"  rel="noopener noreferrer" href="https://www.instagram.com/hamburgueria_kika/"><img src={instagramIcon} alt="Icone link do instagram"/></a>
                 </div>
             </div>

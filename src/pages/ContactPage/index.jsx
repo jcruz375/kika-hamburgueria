67e3fef84184a6265b7ccc47be0d7ba39d836634
João Vitor Oliveira Cruz <61796057+jcruz375@users.noreply.github.com>
2020-09-09 20:30:30 -0300

    Página de contatos

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import PageRoot from '../../components/PageRoot';
 
-//import './styles.css';
+import './styles.css';
 
 
-function ContactPage(){
-    return(
+function ContactPage() {
+    return (
         <PageRoot>
-            <h1>Pagina de Contatos</h1>
+            <div id="contactPage-content" className="container">
+                <h1>Nossos contatos:</h1>
+                <ul>
+                    <li> 
+                        WhatsApp pelo número: (11)96802-5673 ou pelo 
+                        <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5511968025673">whatsApp</a> 
+                    </li>
+                    <li>
+                        Instagram: @hamburgueria_kika
+                        <a href="http://localhost:3000/static/media/instagram.f8c4f0c1.svg">Instagram</a>
+                    </li>
+                    <li>
+                        E-mail: hamburgueriakika@gmail.com
+                    </li>
+                </ul>
+                <br/>
+                <hr/>
+                <br/>
+                <br/>
+                
+                <h1>Site desenvolvido por João Vitor Oliveira Cruz</h1>
+                <ul>
+                    <li>
+                        WhatsApp: (11)98840-6431
+                    </li>
+                    <li>
+                        Linkedin: João Vitor Oliveira Cruz
+                        <a href="https://www.linkedin.com/in/joao-cruz375/">Linkedin</a>
+                    </li>
+                    <li>
+                        GitHub: jcruz375
+                        <a href="https://github.com/jcruz375">GitHub</a>
+                    </li>
+                    <li>
+                        E-mail: joaocruz375@gmail.com
+                    </li>
+                </ul>
+
+            </div>
         </PageRoot>
     );
 };

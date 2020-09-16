b7532e7b50d03321ce059143e70329f607c93d0b
João Vitor Oliveira Cruz <61796057+jcruz375@users.noreply.github.com>
2020-09-15 21:56:59 -0300

    Página de delivery

--- src/pages/DeliveryPage/index.jsx
+++ src/pages/DeliveryPage/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import PageRoot from '../../components/PageRoot';
 
-//import './styles.css';
+import './styles.css';
 
 
-function DeliveryPage(){
-    return(
+function DeliveryPage() {
+    return (
         <PageRoot>
-            <h1>Pagina de Delivey</h1>
+            <div id="content" className="container">
+                <h1>Funcionamos apenas com delivery!</h1>
+                <span>
+                    <img 
+                        src="http://maps.googleapis.com/maps/api/staticmap?center=-22.912869, -43.228963&zoom=15&size=250x250"
+                        alt="imagem do mapa"
+                    />
+                </span>
+            </div>
         </PageRoot>
     );
 };

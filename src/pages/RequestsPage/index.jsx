6d2a136c2d9131bcd10e9b20158afb3dacb21c57
João Vitor Oliveira Cruz <61796057+jcruz375@users.noreply.github.com>
2020-09-23 01:55:52 -0300

    página de pedidos

--- src/pages/RequestsPage/index.jsx
+++ src/pages/RequestsPage/index.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
 import PageRoot from '../../components/PageRoot';
 
-//import './styles.css';
+import './styles.css';
 
 
-function RequestsPage(){
-    return(
+function RequestsPage() {
+    return (
         <PageRoot>
-            <h1>Pagina de Pedidos</h1>
+            <div className="container" id="order-data">
+                <form>
+                    <div className="form-group">
+                        <label for="exampleFormControlInput1">Whats-App:</label>
+                        <input
+                            type="text"
+                            className="form-control form-control-lg"
+                            id="exampleFormControlInput1"
+                            placeholder="Número"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlInput1">Endereço:</label>
+                        <input
+                            type="text"
+                            className="form-control form-control-lg"
+                            id="exampleFormControlInput1"
+                            placeholder="Endereço"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlSelect1">Forma de pagamento:</label>
+                        <select className="form-control form-control-lg" id="exampleFormControlSelect1">
+                            <option>Débito</option>
+                            <option>Crédito</option>
+                            <option>Sodexo</option>
+                            <option>Alelo</option>
+                            <option>VR-benefícios</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlTextarea1">Observações:</label>
+                        <textarea className="form-control form-control-lg" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button className="btn btn-outline-warning btn-block">CONCLUIR PEDIDO</button>
+                </form>
+                <br />
+                <br/>
+                <hr />
+                <h1>SEUS PEDIDOS</h1>
+            </div>
         </PageRoot>
     );
 };
